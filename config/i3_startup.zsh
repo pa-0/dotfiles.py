bc0bf80f725c0b8ed52036d4f4ffba211ed6ccbf
Ferran Jovell <fjovell@redpoints.com>
2019-06-21 15:55:07 +0200

    Enhancements, new aliases and fixes.

--- config/i3_startup.zsh
+++ config/i3_startup.zsh
@@ -12,17 +12,14 @@ BUILTINDISPLAYRATE="60.0"
 DISPLAYS=(`xrandr | awk '/ connected /{printf "%s ", $1}'`)
 
 if [[ "${#DISPLAYS[@]}" == "2" ]]; then
-    XRANDR_OPTIONS="--output \"$EXTERNDISPLAY\" --primary --left-of \"$BUILTINDISPLAY\" \
-        --mode \"$EXTERNDISPLAYRESOLUTION\" \
-        --refresh \"$EXTERNDISPLAYRATE\" \
-        --output \"$BUILTINDISPLAY\" \
-        --mode \"$BUILTINDISPLAYRESOLUTION\" \
-        --refresh \"$BUILTINDISPLAYRATE\""
     echo "Configuring Work display setup"
+    xrandr --output $EXTERNDISPLAY --primary --left-of $BUILTINDISPLAY \
+        --mode $EXTERNDISPLAYRESOLUTION \
+        --refresh $EXTERNDISPLAYRATE \
+        --output $BUILTINDISPLAY \
+        --mode $BUILTINDISPLAYRESOLUTION \
+        --refresh $BUILTINDISPLAYRATE
 else
-    XRANDR_OPTIONS="--auto"
     echo "Configuring auto"
+    xrandr --auto
 fi
-
-# Xandr command to run at startup
-xrandr $XRANDR_OPTIONS
