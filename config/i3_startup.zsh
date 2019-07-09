b32de9732995dc17aef68bcdbf8731e09609650f
Ferran Jovell <fjovell@redpoints.com>
2019-07-09 08:53:11 +0200

    New display setup.

--- config/i3_startup.zsh
+++ config/i3_startup.zsh
@@ -5,7 +5,7 @@ DISPLAYS=(`xrandr | awk '/ connected /{printf "%s ", $1}'`)
 if [[ "${#DISPLAYS[@]}" == "2" ]]; then
     EXTERNDISPLAY=$DISPLAYS[-1]
     BUILTINDISPLAY=$DISPLAYS[1]
-    xrandr --output $EXTERNDISPLAY --primary --left-of $BUILTINDISPLAY --output $BUILTINDISPLAY
+    xrandr --output $EXTERNDISPLAY --primary --right-of $BUILTINDISPLAY --output $BUILTINDISPLAY
     echo "Configuring $EXTERNDISPLAY and $BUILTINDISPLAY display setup"
 else
     xrandr --auto
