cb3afc665e791257605b7d1e5e8609020aab90ad
Ferran Jovell <fjovell@redpoints.com>
2019-08-19 11:56:16 +0200

    Moved screens config to rc and removed it from script.

--- i3/screens.zsh
+++ i3/screens.zsh
@@ -1,10 +1,6 @@
 #!/bin/zsh
 
-DISPLAYS=(`xrandr -q | awk '/ connected /{printf "%s ", $1}'`)
-
-if [[ "${#DISPLAYS[@]}" == "2" ]]; then
-    EXTERNDISPLAY=$DISPLAYS[-1]
-    BUILTINDISPLAY=$DISPLAYS[1]
+if [[ $EXTERNDISPLAY != $BUILTINDISPLAY ]]; then
     xrandr --auto && \
         xrandr --output $EXTERNDISPLAY --primary --right-of $BUILTINDISPLAY \
                --output $BUILTINDISPLAY
