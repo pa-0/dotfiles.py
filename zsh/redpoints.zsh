76079ef6eda3234a3bb1dc68e7d2a260008a2fb6
Ferran Jovell <fjovell@redpoints.com>
2019-08-26 14:13:07 +0200

    Fixed rsgc alias.

--- zsh/redpoints.zsh
+++ zsh/redpoints.zsh
@@ -6,7 +6,7 @@ alias black='black --config $HOME/.config/flake8-black/pyproject.toml'
 
 # DevInt Environment and docker stuff
 alias devint='$HOME/rp/devint-environment/restart.sh -l 3 --proxies-enabled true -e dev --rules-enabled false --bots-config-env local --bots-config-branch develop'
-alias rsgc='`docker run -d --rm --network host --shm-size=1gb selenium/standalone-chrome-debug`'
+alias rsgc='docker run -d --rm --network host --shm-size=1gb selenium/standalone-chrome-debug'
 alias rsff='docker run -d --rm --network host --shm-size=1gb selenium/standalone-firefox-debug:3.7.1-beryllium'
 
 # Pytest Aliases
