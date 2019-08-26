45726b9a7ed6cbf195f8acdf140187d0efc3c1bb
Ferran Jovell <fjovell@redpoints.com>
2019-08-26 11:39:25 +0200

    Changed some aliases to comply with new folder tree.

--- zsh/redpoints.zsh
+++ zsh/redpoints.zsh
@@ -1,3 +1,9 @@
+# Virtualenv aliases
+alias localtestutils='pip uninstall crwtestutils -y && pip install -e $HOME/rp/common/crwtestutils'
+alias localcommon='pip uninstall crwcommon -y && pip install -e $HOME/rp/common/crwcommon'
+alias localebay='pip uninstall crwebaycommon -y && pip install -e $HOME/rp/common/crwebaycommon'
+alias black='black --config $HOME/.config/flake8-black/pyproject.toml'
+
 # DevInt Environment and docker stuff
 alias devint='$HOME/rp/devint-environment/restart.sh -l 3 --proxies-enabled true -e dev --rules-enabled false --bots-config-env local --bots-config-branch develop'
 alias rsgc='`docker run -d --rm --network host --shm-size=1gb selenium/standalone-chrome-debug`'
