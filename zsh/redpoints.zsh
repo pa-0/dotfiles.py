1baf233dd426673f9e7cb29959f825f16f0496f8
Ferran Jovell <fjovell@redpoints.com>
2019-08-29 12:00:01 +0200

    Refactor devint alias to be more flexible.
    devint command now allows an argument which will be the JIRA issue
    number for the branch to fetch for starting devint.

--- zsh/redpoints.zsh
+++ zsh/redpoints.zsh
@@ -1,16 +1,18 @@
-# Virtualenv aliases
-alias localtestutils='pip uninstall crwtestutils -y && pip install -e $HOME/rp/common/crwtestutils'
-alias localcommon='pip uninstall crwcommon -y && pip install -e $HOME/rp/common/crwcommon'
-alias localebay='pip uninstall crwebaycommon -y && pip install -e $HOME/rp/common/crwebaycommon'
+# Redpoints stuff
+RP_COMMON=$HOME/rp/common
+DEVINT_HOME=$HOME/rp/devint-environment
+
+alias localtestutils='pip uninstall crwtestutils -y && pip install -e $RP_COMMON/crwtestutils'
+alias localcommon='pip uninstall crwcommon -y && pip install -e $RP_COMMON/crwcommon'
+alias localebay='pip uninstall crwebaycommon -y && pip install -e $RP_COMMON/crwebaycommon'
 alias black='black --config $HOME/.config/flake8-black/pyproject.toml'
 
-# DevInt Environment and docker stuff
-alias devint='$HOME/rp/devint-environment/restart.sh -l 3 --proxies-enabled true -e dev --rules-enabled false --bots-config-env local --bots-config-branch develop'
+# Selenium dockers
 alias rsgc='docker run -d --rm --network host --shm-size=1gb selenium/standalone-chrome-debug'
 alias rsff='docker run -d --rm --network host --shm-size=1gb selenium/standalone-firefox-debug:3.7.1-beryllium'
 
 # Pytest Aliases
-alias pytall='pytest -nauto -qk "" --cov `pwd | rev | cut -f 1 -d "/" | rev` --cov-report term-missing'
+alias pytall='pytest -nauto -qk "" --cov ${PWD##*/} --cov-report term-missing'
 alias pytdbg='pytest -lvxs'
 
 function deprecate_old_python() {
@@ -23,4 +25,25 @@ function deprecate_old_python() {
 function find_and_rm() {
     sudo find . -type f -name "*.py[co]" -delete 
     sudo find . -type d -name "__pycache__" -delete
+}
+
+function devint() {
+    RESTART_DEVINT=$DEVINT_HOME/restart.sh
+    LEVEL=3
+    PROXIES_ENABLED="true"
+    ENVIRONEMNT="e"
+    RULES="false"
+    BOTS_CONFIG_ENV="local"
+    if [[ -z $1 ]]; then
+        BOTS_CONFIG_BRANCH="develop"
+    else
+        BOTS_CONFIG_BRANCH=BOTSCONFIG-$1
+    fi
+
+    $RESTART_DEVINT -l $LEVEL -e $ENVIRONEMNT \
+        --proxies-enabled $PROXIES_ENABLED \
+        --rules-enabled $RULES \
+        --bots-config-env $BOTS_CONFIG_ENV \
+        --bots-config-branch $BOTS_CONFIG_BRANCH
+    unset RESTART_DEVINT LEVEL PROXIES_ENABLED ENVIRONEMNT RULES BOTS_CONFIG_ENV BOTS_CONFIG_BRANCH
 }
