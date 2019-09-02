da3b5a0faab10bb16276570b39cba33e99fc9a1b
Ferran Jovell <fjovell@redpoints.com>
2019-09-02 14:40:57 +0200

    New alias for rebase interactive to develop.

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,9 +1,9 @@
 # Git aliases
 alias ggrep='git grep -n'
-alias diff='diff --color -u'
 alias gdc='git diff --cached'
 alias gdh='git diff HEAD^!'
 alias gbrs='git branches'
 alias gstap='git stash pop'
 alias gpub='git publish'
 alias gstas='git stash show'
+alias grbid='git rebase --interactive develop'
