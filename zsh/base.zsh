8d0ea36fa0abe9df50961a5c6d22bfbdecd4552a
Ferran Jovell <fjovell@redpoints.com>
2019-09-07 15:54:02 +0200

    Not needed aliases.

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -10,13 +10,6 @@ alias hydra-ssh='ssh -CXY fjovell@hydra.uab.es -p 22'
 alias calc='python -i -c "from math import *"'
 alias rainbow='yes "$(seq 231 -1 16)" | while read i; do printf "\x1b[48;5;${i}m\n"; sleep .02; done'
 
-# Tools 
-alias fzf='fzf --color'
-
-# Pseudo Apps
-alias whatsapp='google-chrome --app=https://web.whatsapp.com/'
-alias gmusic='google-chrome --app=https://music.google.com/'
-
 # Development Aliases
 alias v='nvim'
 alias p='ipython'
