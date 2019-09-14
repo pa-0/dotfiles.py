dbb3f8693d9d799b1cd8544b3fe23c581d23bfb6
Ferran Jovell <fjovell@redpoints.com>
2019-09-14 11:44:02 +0200

    Using my own aliases for dnf.

---
+++ zsh/dnf.zsh
@@ -0,0 +1,9 @@
+# Aliases for dnf
+alias dnfl='dnf list'
+alias dnfli='dnf list installed'
+alias dnfp='dnf info'
+alias dnfs='dnf search'
+
+alias dnfu='sudo dnf update'
+alias dnfi='sudo dnf install'
+alias dnfr='sudo dnf remove'
