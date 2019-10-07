6a1afba8ecb5e84632021ecdb614598c6a92742d
Ferran Jovell <fjovell@redpoints.com>
2019-10-07 14:10:08 +0200

    Added aliases for exa if it's installed otherwise fallback.

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -1,14 +1,22 @@
 # Terminal stuff
 alias sd='sudo '
 alias sv='sudo nvim'
-alias ls='ls --color=auto'
-alias ll='ls -lsF'
+if test exa &> /dev/null; then
+    alias ls='exa'
+    alias ll='exa --icons --long --git'
+    alias lt='exa --icons --tree'
+    alias llrt='ll -s time'
+else
+    alias ls='ls --color=auto'
+    alias ll='ls -lsF'
+fi
+
 alias hydra-ssh='ssh -CXY fjovell@hydra.uab.es -p 22'
 alias calc='python -i -c "from math import *"'
 alias rainbow='yes "$(seq 231 -1 16)" | while read i; do printf "\x1b[48;5;${i}m\n"; sleep .02; done'
 
 # Development Aliases
-alias p='ipython'
+alias p='ipython3'
 alias v='nvim'
 
 # Assign these file extensions to neovim
