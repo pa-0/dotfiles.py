e499576fad3f06c6e006419ddb593fe1ec9618b7
Ferran Jovell <fjovell@redpoints.com>
2019-10-22 15:51:17 +0200

    Slightly different format.

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -2,7 +2,7 @@
 #
 function docker () {
     if [[ $@ == "ps" ]]; then
-        command docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}"
+        command docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Networks}}\t{{.Ports}}"
     else
         command docker "$@"
     fi
