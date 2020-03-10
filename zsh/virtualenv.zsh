c0911ffbf02748ce810a8899dd085688f38cad2d
Ferran Jovell <fjovell@redpoints.com>
2020-03-10 12:13:28 +0100

    Make sure that we export the correct variable for virtualenvwrapper.
    Wether the custom version is compiled or not, we should always export
    something for virtualenvwrapper or it will complain on a new shell every
    time.

--- zsh/virtualenv.zsh
+++ zsh/virtualenv.zsh
@@ -1,11 +1,17 @@
 # VirtualEnvWrapper
 export WORKON_HOME=$HOME/.virtualenv
-export PIP_REQUIRE_VIRTUALENV=true
-export VIRTUALENVWRAPPER_PYTHON=$HOME/.local/install/cpython-3.7.5/bin/python3
 export VIRTUALENVWRAPPER_HOOK_DIR=$DOTFILES/python/virtualenvwrapper/
+
+PYTHON_375=$HOME/.local/install/cpython-3.7.5
+if [[ -d $PYTHON_375 ]]; then
+    export VIRTUALENVWRAPPER_PYTHON=$PYTHON_375/bin/python3
+else
+    export VIRTUALENVWRAPPER_PYTHON=$(which python3)
+fi
 
 [[ -f $(which virtualenvwrapper.sh 2> /dev/null ) ]] && source $(which virtualenvwrapper.sh)
 
+export PIP_REQUIRE_VIRTUALENV=true
 alias gpip='PIP_REQUIRE_VIRTUALENV="" sudo python3 -m pip'
 alias pl='python3 -m pip list'
 
