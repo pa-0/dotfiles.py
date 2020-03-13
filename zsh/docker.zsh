ba263b1d79ac88ebd8c7ee5d99003a40fbaddf95
Ferran Jovell <fjovell@redpoints.com>
2020-03-13 12:19:36 +0100

    Added a stop all subcommands for docker

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -3,6 +3,8 @@
 function docker () {
     if [[ $@ == "ps" ]]; then
         command docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Networks}}\t{{.Ports}}"
+    elif [[ $@ == "stop all" ]]; then
+        command docker stop $(docker ps -q)
     else
         command docker "$@"
     fi
