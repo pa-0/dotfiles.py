0b52af31088ca9be769dc21a4ccb8bbcaebc22d6
Ferran Jovell <fjovell@redpoints.com>
2020-04-08 18:02:52 +0200

    Two new aliases for git

--- zsh/config.d/git.zsh
+++ zsh/config.d/git.zsh
@@ -38,6 +38,8 @@ alias gcl='git clone'
 
 alias gco='git checkout'
 alias gsw='git switch'
+alias gswd='git switch develop'
+alias gsw-='git switch -'
 alias gr='git restore'
 alias gra='git restore .'
 alias grs='git restore --staged'
