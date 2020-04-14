429af1029573fdc947ab1906f9461b71e11dafd3
Ferran Jovell <fjovell@redpoints.com>
2020-04-15 00:42:41 +0200

    [fzf] Added a select-1 in the tm function

--- zsh/config.d/fzf.zsh
+++ zsh/config.d/fzf.zsh
@@ -23,7 +23,7 @@ tm () {
     fi
 
     session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | \
-        fzf --exit-0 \
+        fzf --select-1 --exit-0 \
     ) && \
     tmux $change -t "$session" || echo "No sessions found."
 }
