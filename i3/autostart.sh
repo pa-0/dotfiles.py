885959720bb8ff86ef400ffb49154d1ac577ebb3
Ferran Jovell <fjovell@redpoints.com>
2020-04-15 14:42:26 +0200

    [i3/autostart] POSIX compliance with autostart script

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -5,7 +5,7 @@ export DOTFILES=$HOME/.dotfiles
 
 # Reload screens
 killall -wq enact
-if [[ $(command -v enact) ]]
+if [ "$(command -v "$HOME/.local/cargo/bin/enact")" ]
 then
     enact --pos left --watch &
 fi
@@ -13,19 +13,19 @@ fi
 # Launch polybar
 killall -wq polybar
 
-if [[ $(command -v xrandr) ]]
+if [ "$(command -v xrandr)" ]
 then
     for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$monitor polybar -c $DOTFILES/i3/polybar/config --reload fjovell &
+        MONITOR=$monitor polybar -c "$DOTFILES/i3/polybar/config" --reload fjovell &
     done
 else
-    polybar -c $DOTFILES/i3/polybar/config --reload fjovell &
+    polybar -c "$DOTFILES/i3/polybar/config" --reload fjovell &
 fi
 
 # Redshift
 killall -wq redshift
 
-if [[ $(command -v redshift) ]]
+if [ "$(command -v redshift)" ]
 then
     redshift &
 fi
@@ -33,19 +33,19 @@ fi
 # Dunst
 killall -wq dunst
 
-if [[ $(command -v dunst) ]]
+if [ "$(command -v dunst)" ]
 then
-    dunst -config $DOTFILES/i3/dunst/dunstrc &
+    dunst -config "$DOTFILES/i3/dunst/dunstrc" &
 fi
 
 # Keyboard stuff
-if [[ $(command -v setxkbmap) ]]
+if [ "$(command -v setxkbmap)" ]
 then
     setxkbmap -layout "us,es" -option "grp:alt_shift_toggle" -option "caps:escape"
 fi
 
 # background
-if [[ $(command -v feh) ]]
+if [ "$(command -v feh)" ]
 then
-    feh --bg-scale $DOTFILES/wallpaper/wallpaper.jpg
+    feh --bg-scale "$DOTFILES/wallpaper/wallpaper.jpg"
 fi
