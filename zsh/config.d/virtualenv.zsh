a5d6d28eba9c7c5c37fe4a2e66f504979fee62ec
Ferran Jovell <fjovell@redpoints.com>
2020-05-02 14:14:22 +0200

    [virtualenv] New OS without compiled python version.

--- zsh/config.d/virtualenv.zsh
+++ zsh/config.d/virtualenv.zsh
@@ -1,17 +1,9 @@
 # VirtualEnvWrapper
 WORKON_HOME=$HOME/.local/virtualenv
 VIRTUALENVWRAPPER_HOOK_DIR=$DOTFILES/python/virtualenvwrapper/
-PYTHON_375=$HOME/.local/install/cpython-3.7.5
-
-if [[ -d $PYTHON_375 ]]; then
-    VIRTUALENVWRAPPER_PYTHON=$PYTHON_375/bin/python3
-else
-    VIRTUALENVWRAPPER_PYTHON=$(command -v python3)
-fi
-
-[[ -f $(command -v virtualenvwrapper_lazy.sh) ]] && source $(command -v virtualenvwrapper_lazy.sh)
-
+VIRTUALENVWRAPPER_PYTHON=$(command -v python)
 PIP_REQUIRE_VIRTUALENV=true
+[[ -f $(command -v virtualenvwrapper_lazy.sh) ]] && source $(command -v virtualenvwrapper_lazy.sh)
 
 export WORKON_HOME VIRTUALENVWRAPPER_HOOK_DIR VIRTUALENVWRAPPER_PYTHON PIP_REQUIRE_VIRTUALENV
 
