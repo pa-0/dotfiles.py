202e99ffdae0dcabecacf22c0de65b554406ff98
Ferran Jovell <ferran.jovell@gmail.com>
2020-08-19 08:47:10 +0200

    [screens] Small screens refactor

--- i3/screens.sh
+++ i3/screens.sh
@@ -24,30 +24,35 @@ two_screen () {
     xrandr --output "$EXTERNAL_SCREEN" --primary --output "$LAPTOP_SCREEN" --"$POSITION"-of "$LAPTOP_SCREEN"
 }
 
-while getopts "lea" ARG
-do
-    case $ARG in
-        l )
-            only_laptop_screen
-            ;;
-        e )
-            only_external_screen
-            ;;
-        a )
-            two_screen
-            ;;
-        \? )
-            echo "Option Unrecognized"
-            echo "Usage:"
-            echo "  $0 -lea"
-            echo "  -l: Only laptop screen."
-            echo "  -e: Only external screen."
-            echo "  -a: All Screens."
-            echo
-            exit 1
-            ;;
-    esac
-done
+screens ()
+{
+    while getopts "lea" ARG
+    do
+        case $ARG in
+            l )
+                only_laptop_screen
+                ;;
+            e )
+                only_external_screen
+                ;;
+            a )
+                two_screen
+                ;;
+            \? )
+                echo "Option Unrecognized"
+                echo "Usage:"
+                echo "  $0 -lea"
+                echo "  -l: Only laptop screen."
+                echo "  -e: Only external screen."
+                echo "  -a: All Screens."
+                echo
+                exit 1
+                ;;
+        esac
+    done
 
-i3-msg reload
-i3-msg restart
+    i3-msg reload
+    i3-msg restart
+}
+
+Screens "$@"
