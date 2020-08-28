812e3217f6be8c35c800a72c616d00fd123c01b4
Ferran Jovell <ferran.jovell@gmail.com>
2020-08-28 17:32:17 +0200

    [screens] Bye bye secondary screen

--- screens/main.sh
+++ screens/main.sh
@@ -3,5 +3,4 @@
 # shellcheck disable=SC1090
 source "$HOME/.zshenv"
 
-xrandr --output "$MAIN_SCREEN" --primary --mode 2560x1440 --pos 1920x0 --rotate normal \
-    --output "$VERTICAL_SCREEN" --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output "$MAIN_SCREEN" --primary --mode 2560x1440 --pos 1920x0 --rotate normal
