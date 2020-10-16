47b0cc2ee339af93de7636e672dc4f0cfb7bf965
Ferran Jovell <ferran.jovell@gmail.com>
2020-10-16 14:11:05 +0200

    [screens] Changed Refresh rate of main monitor from 60 to 144 Hz

--- screens/dual.sh
+++ screens/dual.sh
@@ -3,5 +3,5 @@
 # shellcheck disable=SC1090
 source "$HOME/.zshenv"
 
-xrandr --output "$MAIN_SCREEN" --primary --mode 2560x1440 --rate 60 --pos 2560x0 --rotate normal \
+xrandr --output "$MAIN_SCREEN" --primary --mode 2560x1440 --rate 144.0 --pos 2560x0 --rotate normal \
     --output "$SECONDARY_SCREEN" --mode 2560x1440 --pos 0x0 --rotate normal --rate 60
