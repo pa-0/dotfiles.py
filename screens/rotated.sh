d3f49516106a2e1e7320c54687ca476ef5a25ebe
Ferran Jovell <ferran.jovell@gmail.com>
2020-10-26 10:02:01 +0100

    [screens] Changed config for rotated screen.

--- screens/rotated.sh
+++ screens/rotated.sh
@@ -3,5 +3,5 @@
 # shellcheck disable=SC1090
 source "$HOME/.zshenv"
 
-xrandr --output "$MAIN_SCREEN" --primary --mode 2560x1440 --rate 60 --pos 1440x743 --rotate normal \
-    --output "$SECONDARY_SCREEN" --mode 2560x1440 --pos 0x0 --rotate left --rate 60
+xrandr --output "$MAIN_SCREEN" --primary --mode 2560x1440 --rate 144.0 --pos 1440x743 --rotate normal \
+    --output "$SECONDARY_SCREEN" --mode 2560x1440 --pos 0x0 --rotate right --rate 60
