2552689b2b68bbcd8e567d98fd3d6a4b50b8adf6
Ferran Jovell <ferran.jovell@gmail.com>
2021-02-02 16:42:29 +0100

    [kubectl/zsh] Moving kubectl config into its own directory

---
+++ kubectl/init.zsh
@@ -0,0 +1,10 @@
+# Kubectl config
+
+export KUBECONFIG="$HOME/.config/kube/config"
+export KREW_ROOT="$HOME/.local/share/krew"
+
+# TODO: Find what fixes this problem :(
+if [[ "$(command -v kubectl)" ]]
+then
+    source <(kubectl completion zsh)
+fi
--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,11 +1,5 @@
 # Completions for varied CLI programs
 
-# TODO: Find what fixes this problem :(
-if [[ "$(command -v kubectl)" ]]
-then
-    source <(kubectl completion zsh)
-fi
-
 if [[ "$(command -v pipx)" ]]
 then
     eval "$(register-python-argcomplete pipx)"
