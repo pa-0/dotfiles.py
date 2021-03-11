d2f15fd205365a413f5486cad13c3f0d8893caac
Ferran Jovell <ferran.jovell@gmail.com>
2021-03-11 22:42:54 +0100

    [i3/screens] Added a vertical only mode

---
+++ screens/vertical.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+# shellcheck disable=SC1090
+source "$HOME/.zshenv"
+
+xrandr --output "$MAIN_SCREEN" --off\
+    --output "$SECONDARY_SCREEN" --mode 2560x1440 --pos 0x0 --rotate right --rate 60
