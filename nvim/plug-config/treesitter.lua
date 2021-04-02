f991682e3e7f95e099d11bf4dc503ad60824fbc1
Ferran Jovell <ferran.jovell@gmail.com>
2021-04-02 07:46:33 +0200

    [nvim] Adding incremental_selection to treesitter plugin

--- nvim/plug-config/treesitter.lua
+++ nvim/plug-config/treesitter.lua
@@ -16,6 +16,15 @@ require "nvim-treesitter.configs".setup {
         "toml",
         "yaml"
     },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm"
+        }
+    },
     highlight = {
         enable = true
     },
