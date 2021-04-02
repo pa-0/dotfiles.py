8cc0b54cd7ed3be83343d3c79a36dafee760b93e
Ferran Jovell <ferran.jovell@gmail.com>
2021-04-02 07:41:34 +0200

    [nvim] Additional configuration for kommentary

--- nvim/plug-config/kommentary.lua
+++ nvim/plug-config/kommentary.lua
@@ -1,1 +1,25 @@
 require "kommentary.config".use_extended_mappings()
+
+require "kommentary.config".configure_language(
+    "lua",
+    {
+        single_line_comment_string = "--",
+        prefer_single_line_comments = true
+    }
+)
+
+require "kommentary.config".configure_language(
+    "go",
+    {
+        single_line_comment_string = "//",
+        prefer_single_line_comments = true
+    }
+)
+
+require "kommentary.config".configure_language(
+    "rust",
+    {
+        single_line_comment_string = "//",
+        prefer_single_line_comments = true
+    }
+)
