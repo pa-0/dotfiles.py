0395796cba0bac2b0755b6b03da856806979264b
Ferran Jovell <ferran.jovell@gmail.com>
2021-04-09 22:25:03 +0200

    [screens] Correct screen rotation after changing screens.

--- screens/vertical.sh
+++ screens/vertical.sh
@@ -4,4 +4,4 @@
 source "$HOME/.zshenv"
 
 xrandr --output "$MAIN_SCREEN" --off\
-    --output "$SECONDARY_SCREEN" --mode 2560x1440 --pos 0x0 --rotate right --rate 60
+    --output "$SECONDARY_SCREEN" --mode 2560x1440 --pos 0x0 --rotate left --rate 60
