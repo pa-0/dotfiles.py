778779c9bcd78cc08615efe659804d7c5120a389
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-08-06 10:25:42 +0200

    [nvim] Disables builtin plugins

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -70,3 +70,29 @@ vim.opt.listchars = {
 }
 
 vim.opt.showbreak = "↪"
+
+-- Disable some in built plugins completely
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+    --'matchparen',
+}
+for _, plugin in pairs(disabled_built_ins) do
+    vim.g["loaded_" .. plugin] = 1
+end
