07a24ad9458b506288f3d9dcd631c6f31d771282
Ferran Jovell <ferran@jovell.dev>
2021-08-11 21:18:26 +0200

    [zsh] Moved zsh keybindings to file
    This is much better because all the keybinding related options to be in
    the same file.

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -4,3 +4,6 @@ bindkey '^ ' autosuggest-accept
 # Allows accepting tab suggestions with <space>
 zmodload zsh/complist
 bindkey -M menuselect ' ' accept-search
+
+# Edit line in vim with ctrl-e:
+autoload edit-command-line; zle -N edit-command-line; bindkey '^x^e' edit-command-line
