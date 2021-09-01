6d737809492fab74b1edd1ec17aefce4b35d8187
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-09-01 17:19:39 +0200

    [nvim] Adds config for lasplace

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -44,7 +44,10 @@ require("packer").startup {
         use {
             "ethanholz/nvim-lastplace",
             config = function()
-                require("nvim-lastplace").setup()
+                require("nvim-lastplace").setup {
+                    lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+                    lastplace_ignore_filetype = {"gitcommit", "gitrebase"}
+                }
             end
         }
 
