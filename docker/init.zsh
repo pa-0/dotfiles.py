cf03010937b2422f3104781085c4448e6b203741
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-09-17 12:41:04 +0200

    [docker/zsh] Format file with shfmt

--- docker/init.zsh
+++ docker/init.zsh
@@ -4,15 +4,15 @@ export DOCKER_BUILDKIT=1
 
 # Alias docker commands
 if [ "$(command -v docker)" ]; then
-	docker() {
-		if [[ $@ == "ps" ]]; then
-			command docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Networks}}\t{{.Ports}}"
-		elif [[ $@ == "stop all" ]]; then
-			command docker stop $(docker ps -q)
-		else
-			command docker "$@"
-		fi
-	}
+    docker() {
+        if [[ $@ == "ps" ]]; then
+            command docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Networks}}\t{{.Ports}}"
+        elif [[ $@ == "stop all" ]]; then
+            command docker stop $(docker ps -q)
+        else
+            command docker "$@"
+        fi
+    }
 fi
 
 alias dps='docker ps'
