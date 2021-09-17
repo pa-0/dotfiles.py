78841195b46261cfe0241924010a53fcc4e91d9a
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-09-17 12:09:42 +0200

    [zsh/docker] Adds aliases for common docker commands

--- docker/init.zsh
+++ docker/init.zsh
@@ -3,15 +3,18 @@
 export DOCKER_BUILDKIT=1
 
 # Alias docker commands
-if [ "$(command -v docker)" ]
-then
-    docker () {
-        if [[ $@ == "ps" ]]; then
-            command docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Networks}}\t{{.Ports}}"
-        elif [[ $@ == "stop all" ]]; then
-            command docker stop $(docker ps -q)
-        else
-            command docker "$@"
-        fi
-    }
+if [ "$(command -v docker)" ]; then
+	docker() {
+		if [[ $@ == "ps" ]]; then
+			command docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Status}}\t{{.Names}}\t{{.Networks}}\t{{.Ports}}"
+		elif [[ $@ == "stop all" ]]; then
+			command docker stop $(docker ps -q)
+		else
+			command docker "$@"
+		fi
+	}
 fi
+
+alias dps='docker ps'
+alias dcb='docker-compose build --parallel'
+alias dcd='docker-compose down --remove-orphans'
