67cd37d54261b2131ce9840318375aa4f70c47b9
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-10-06 09:15:36 +0200

    [zsh] sh formatting

--- zsh/custom_functions.zsh
+++ zsh/custom_functions.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 
 function public-ip() {
-	local PUBLIC_IP=$(curl -sL v4.ifconfig.co | awk '{ print $0 "/32" }')
-	echo $PUBLIC_IP
+    local PUBLIC_IP=$(curl -sL v4.ifconfig.co | awk '{ print $0 "/32" }')
+    echo $PUBLIC_IP
 }
 
 function src() {
