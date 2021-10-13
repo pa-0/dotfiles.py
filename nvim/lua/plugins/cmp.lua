8f69ec14f1821ac04fb6aa620a708fcb61d87d45
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-10-13 11:40:15 +0200

    [nivm] Adapt to breaking changes for nvim-cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,31 +1,32 @@
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 vim.o.completeopt = "menuone,noselect"
 
 cmp.setup {
     snippet = {
         expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
         end
     },
     mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Replace, select = true}),
-        ["<Tab>"] = function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+        ["<c-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}),
+        ["<c-n>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}),
+        ["<c-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<c-f>"] = cmp.mapping.scroll_docs(4),
+        ["<c-space>"] = cmp.mapping.complete(),
+        ["<c-e>"] = cmp.mapping.close(),
+        ["<cr>"] = cmp.mapping.confirm({behavior = cmp.ConfirmBehavior.Replace, select = true}),
+        ["<tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
             else
                 fallback()
             end
         end,
-        ["<S-Tab>"] = function(fallback)
-            if vim.fn.pumvisible() == 1 then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+        ["<s-tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
             else
                 fallback()
             end
