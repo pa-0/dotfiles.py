402d3557d101540d7545c110a57bbe4396aaa719
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-10-21 10:30:30 +0200

    [nvim] Disable unused providers

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -101,3 +101,9 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
     vim.g["loaded_" .. plugin] = 1
 end
+
+-- Deactivate unused providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider = 0
