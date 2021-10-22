0f5cd73828f20da76ac6c6e2f813a74e68c9d4b0
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-10-22 15:17:59 +0200

    [nvim] Formatting on mappings.lua

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,10 +4,8 @@ vim.g.mapleader = ","
 
 nest.applyKeymaps({
     {
-        mode = "n",
-        {
-            "<leader>",
-            {
+        mode = "n", {
+            "<leader>", {
                 { "w", "<cmd>w<cr>" },
                 { "W", "<cmd>wa!<cr>" },
                 { "e", "<cmd>wq!<cr>" },
@@ -18,31 +16,27 @@ nest.applyKeymaps({
                 { "s", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
                 { "l", "<cmd> lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>" },
                 {
-                    "t",
-                    {
+                    "t", {
                         { "n", "<cmd>TestNearest<CR>" },
                         { "f", "<cmd>TestFile<CR>" },
                     },
                 },
                 {
-                    "g",
-                    {
+                    "g", {
                         { "s", "<cmd>Neogit<cr>" },
                         { "b", "<cmd>lua require('telescope.builtin').git_branches()<cr>" },
                         { "c", "<cmd>lua require('telescope.builtin').git_commits()<cr>" },
                     },
                 },
                 {
-                    "v",
-                    {
+                    "v", {
                         { "o", "<cmd>VimuxOpenRunner<CR>" },
                         { "c", "<cmd>VimuxCloseRunner<CR>" },
                         { "l", "<cmd>VimuxClearTerminalScreen<CR>" },
                     },
                 },
                 {
-                    "d",
-                    {
+                    "d", {
                         { "o", "<cmd>DiffviewOpen<cr>" },
                         { "c", "<cmd>DiffviewClose<cr>" },
                         { "h", "<cmd>DiffviewFileHistory<cr>" },
@@ -52,18 +46,15 @@ nest.applyKeymaps({
         },
         {
             -- Figure out why not working
-            "\\",
-            {
+            "\\", {
                 { "\\", "<cmd>NvimTreeToggle<cr>" },
                 { "f", "<cmd>NvimTreeFindFile<cr>" },
                 { "q", "<cmd>NvimTreeClose<cr>" },
             },
         },
         {
-            "g",
-            {
-                "<c-",
-                {
+            "g", {
+                "<c-", {
                     { "l>", "<cmd>lua require(\"swap-buffers\").swap_buffers(\"l\")<cr>" },
                     { "k>", "<cmd>lua require(\"swap-buffers\").swap_buffers(\"k\")<cr>" },
                     { "j>", "<cmd>lua require(\"swap-buffers\").swap_buffers(\"j\")<cr>" },
@@ -72,8 +63,7 @@ nest.applyKeymaps({
             },
         },
         {
-            "<c-",
-            {
+            "<c-", {
                 { "e>", "3<c-e>" },
                 { "y>", "3<c-y>" },
                 { "h>", "<cmd>TmuxNavigateLeft<CR>" },
@@ -97,28 +87,24 @@ nest.applyKeymaps({
         options = { noremap = true, silent = true },
     },
     {
-        mode = "i",
-        {
+        mode = "i", {
             { "<c-r>", "<cmd>lua require(\"telescope.builtin\").registers()<cr>" },
         },
     },
     {
-        mode = "v",
-        {
+        mode = "v", {
             { "<", "<gv" },
             { ">", ">gv" },
         },
     },
     {
-        mode = "c",
-        {
+        mode = "c", {
             { "<c-a>", "<home>" },
             { "<c-e>", "<end>" },
         },
     },
     {
-        mode = "t",
-        {
+        mode = "t", {
             { "<c-h>", "<-\\><c-n><c-w><c-h>" },
             { "<c-j>", "<-\\><c-n><c-w><c-j>l" },
             { "<c-k>", "<-\\><c-n><c-w><c-k>l" },
