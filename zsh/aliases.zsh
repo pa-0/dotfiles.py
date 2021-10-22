1e3aab47a2cffbd46c4e974ff9afc55d22272893
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-10-22 10:20:15 +0200

    [zsh] Adds groot alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,26 +8,53 @@ alias cl='clear'
 alias copy='xsel -i --clipboard' # Meant to use it as a pipe
 alias cd-='cd -'
 
+function sudo {
+    echo -e "\e[0;32m"
+    echo -e "⠀⠀⠀⠀⠀⠀⡾⠛⠳⣄⠀⣠⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⠀⠀⠀⣧⠘⣆⣿⠟⢹⠈⠁⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⣠⣄⡀⠀⣠⣬⡓⡿⡏⠀⢸⠀⠀⡏⠸⣦⡴⣿⠛⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⢧⡐⣽⠟⠉⡟⠙⠃⡇⠀⠀⠀⠀⠀⠀⡽⠀⠀⠀⡏⣘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠉⢻⡇⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠇⢸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀\e[0;33m"
+    echo -e "⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⢸⣇\e[0;34m ⡴⠛⣿⣦⠀⠀⠀⠀⠀⠀⢀⡞⠙⣿⣦⠀\e[0;33m⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⢸⡿\e[0;34m ⣷⣶⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⣶⣿⡿⠁\e[0;33m⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⢸⡇\e[0;34m⠀⠈⠉⠉⠁\e[0;33m⠀⢰⣶⣶⣶⣶⣶⠀\e[0;34m⠀⠀⠈⠉⠉⠀\e[0;33m⠀⣄⣿⠀⠀⠀⠀⠀\e[0;93m⠀⠀⠀⠀⠀⠀⠀I am (G)root   \e[0;33m"
+    echo -e "⠀⠀⠀⠀⠻⣴⡀⠀⠀⠈⠛⠿⠭⠔⠋⠀⠀⠀⣄⣰⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⠀⠀⠈⠛⠿⠷⠤⣶⣤⣤⣤⣤⡾⠾⠞⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⢠⡞⠳⣄⣀⣀⡴⢾⡷⣄⡇⢸⣿⢟⣲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⠈⠳⣤⣼⣟⣩⡽⣿⠁⢮⠳⣼⠟⠛⣦⢉⣧⣤⡀⠀⠀⠀⠀⠀\e[3;32m    With great power comes great responsibility       \e[0;33m"
+    echo -e "⠀⠀⠀⠀⠀⠀⠉⠉⠀⢠⡿⢤⡈⢧⡟⠀⠀⠈⠿⣤⡼⠇⠀⠀⠀⠀⠀\e[3;32m    Use sudo wisely.                                  \e[0;33m"
+    echo -e "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⠄⠙⠻⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\e[0;37m"
+    echo -e "⠀⠀⠀⠀⠀⠀⠀⣶⣶⣾⣷⣶⣶⣿⣿⣿⣶⣶⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e "⠀⠀⠀⠀⠀⠀⠀⠈⠿⢿⣿⣿⣿⣿⣿⣿⡿⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+    echo -e ""
+    command sudo "$@"
+
+}
+
 # Go To dotfiles location
 alias dot='cd $DOTFILES'
 alias dev='cd $DEV'
 
 # Make it easy to call nvim
 if [ "$(command -v nvim)" ]; then
-	alias e="$EDITOR --noplugins"
-	alias v="$EDITOR"
-	alias vim="$EDITOR"
+    alias e="$EDITOR --noplugins"
+    alias v="$EDITOR"
+    alias vim="$EDITOR"
 fi
 
 # Replace ls for exa
 if [ "$(command -v exa)" ]; then
-	alias ls='exa'
-	alias ll='exa --icons --long --git'
-	alias lt='exa --icons --tree --git-ignore'
-	alias lr='exa --icons --long --git --sort time'
+    alias ls='exa'
+    alias ll='exa --icons --long --git'
+    alias lt='exa --icons --tree --git-ignore'
+    alias lr='exa --icons --long --git --sort time'
 else
-	alias ls='ls --color=auto'
-	alias ll='ls -lsF'
+    alias ls='ls --color=auto'
+    alias ll='ls -lsF'
 fi
 
 # Replace cat for bat
@@ -35,17 +62,17 @@ fi
 
 # Aliases for dnf
 if [ "$(command -v dnf)" ]; then
-	alias dnfl='dnf list'
-	alias dnfli='dnf list installed'
-	alias dnfp='dnf info'
-	alias dnfs='dnf search'
-	alias dnfw='dnf whatprovides'
+    alias dnfl='dnf list'
+    alias dnfli='dnf list installed'
+    alias dnfp='dnf info'
+    alias dnfs='dnf search'
+    alias dnfw='dnf whatprovides'
 
-	alias dnfu='sudo dnf update'
-	alias dnfy='sudo dnf update -y'
-	alias dnfi='sudo dnf install'
-	alias dnfyy='sudo dnf install -y'
-	alias dnfr='sudo dnf remove'
+    alias dnfu='sudo dnf update'
+    alias dnfy='sudo dnf update -y'
+    alias dnfi='sudo dnf install'
+    alias dnfyy='sudo dnf install -y'
+    alias dnfr='sudo dnf remove'
 fi
 
 alias wallpaper='nitrogen --restore'
