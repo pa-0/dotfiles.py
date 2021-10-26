449fb94351a7604886bb5f5e9ae865f0e0e937d4
Ferran Jovell <ferran.jovell@hybridtheory.com>
2021-10-26 20:07:43 +0200

    [nvim/lsp] Changing order of sources for null_ls
    Changing order for sources in null_ls as they are applied in the order
    they are registered. This way, black config will prevail over isort.

--- nvim/lua/plugins/null_ls.lua
+++ nvim/lua/plugins/null_ls.lua
@@ -4,30 +4,30 @@ local M = {}
 
 local function register_null_ls_sources()
     local sources = {
-        null_ls.builtins.diagnostics.flake8.with({
-            extra_args = {
-                "--config",
-                vim.fn.expand("$DOTFILES/python/flake8", "--max-line-length", vim.fn.expand("$PYTHON_LINE_LENGTH")),
-            },
-        }),
-        null_ls.builtins.diagnostics.shellcheck.with({ filetypes = { "sh", "zsh" } }),
+        -- Python
+        null_ls.builtins.diagnostics.flake8,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black,
+        -- Lua
         null_ls.builtins.diagnostics.selene.with({
             extra_args = { "--config", vim.fn.expand("$DOTFILES/selene.toml") },
         }),
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
         null_ls.builtins.diagnostics.yamllint.with({
             extra_args = { "-c", vim.fn.expand("$DOTFILES/python/yamllint.yml") },
         }),
-        null_ls.builtins.formatting.black.with({
-            extra_args = { "--config", vim.fn.expand("$DOTFILES/python/black-config.toml") },
-        }),
-        null_ls.builtins.formatting.eslint_d,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.shfmt.with({ filetypes = { "sh", "zsh" } }),
+        -- Javascript
         null_ls.builtins.formatting.stylelint,
-        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.eslint_d,
+        null_ls.builtins.formatting.prettierd,
+        -- Golang
+        null_ls.builtins.formatting.gofmt,
+        -- Rust
+        null_ls.builtins.formatting.rustfmt,
+        -- Shell
+        null_ls.builtins.diagnostics.shellcheck.with({ filetypes = { "sh", "zsh" } }),
+        -- Common
         null_ls.builtins.formatting.trim_newlines,
         null_ls.builtins.formatting.trim_whitespace,
     }
