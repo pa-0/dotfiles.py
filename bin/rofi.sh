991003d23509518ce8b6ef04472256b11d128abf
Ferran Jovell <ferran@jovell.dev>
2021-12-30 18:06:23 +0100

    [i3/rofi] Formatting + reduce number of lines

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -3,9 +3,8 @@
 # shellcheck disable=SC1090
 source "$HOME/.zshenv"
 
-launch_rofi ()
-{
-    rofi -modi drun -show drun -lines 7
+launch_rofi() {
+    rofi -modi drun -show drun -lines 5
 }
 
 launch_rofi "$@"
