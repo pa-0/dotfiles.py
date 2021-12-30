f6eec1fb8156cb450bb313a9eb031c66e0f07e53
Ferran Jovell <ferran@jovell.dev>
2021-12-30 16:55:40 +0100

    [zsh/fzf] Improvements on fzf.zsh

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -17,15 +17,15 @@ is_in_git_repo() {
     git rev-parse --is-inside-work-tree &>/dev/null
 }
 
-_git_restore_files() {
-    # TODO: Add scrolling for the preview
-    git status -s | grep -oP "$RE_FILES" | _fzf_window --header "Select a file" --layout=reverse -m --preview-window=right:80% $@
-}
 # Default FZF opts and parameters
 _fzf_window() {
     local query
     [[ "$querystring" ]] && query="-q ${querystring}"
-    fzf-tmux --exit-0 --select-1 --preview $preview_cmd --select-1 $query $@
+    fzf-tmux --exit-0 --select-1 --preview "$preview_cmd" --select-1 $query "$@"
+}
+
+_git_restore_files() {
+    git status -s | grep -oP "$RE_FILES" | _fzf_window --header "Select a file" --layout=reverse -m --preview-window=right:80%
 }
 
 _fzf_choose_branch() {
@@ -41,65 +41,72 @@ _fzf_choose_branch() {
 if [ "$(command -v tmux)" ]; then
     tm() {
         local TMUX_CONFIG=$DOTFILES/tmux/tmux.conf
+        local session
+        session=$1
         [[ -n $TMUX ]] && change="switch-client" || change="attach-session"
-        if [ $1 ]; then
-            tmux -f $TMUX_CONFIG $change -t "$1" 2>/dev/null ||
-                (tmux -f $TMUX_CONFIG new-session -d -s $1 &&
-                    tmux -f $TMUX_CONFIG $change -t "$1")
-            return
-        fi
-
-        if [ "$(command -v tmuxp)" ]; then
-            # If no session available and tmuxp is, load from config
-            tmuxp load $DOTFILES/tmux/sessions/fjm.yml $DOTFILES/tmux/sessions/wrk.yml
+        if [ "$session" ]; then
+            tmux -f "$TMUX_CONFIG" "$change" -t "$1" 2>/dev/null ||
+                (tmux -f "$TMUX_CONFIG" new-session -d -s "$1" &&
+                    tmux -f "$TMUX_CONFIG" "$change" -t "$1")
             return
         fi
 
         # otherwise, use the usual fzf script to attach to one or complaint there's no sessions
         session=$(
             tmux -f TMUX_CONFIG list-sessions -F "#{session_name}" 2>/dev/null |
-                fzf --select-1 --exit-0 --height $(tmux -f $TMUX_CONFIG list-sessions | wc -l)
+                fzf --select-1 --exit-0 --height "$(tmux -f "$TMUX_CONFIG" list-sessions | wc -l)"
         ) &&
-            tmux -f $TMUX_CONFIG $change -t "$session" || echo "No sessions found."
+            tmux -f "$TMUX_CONFIG" "$change" -t "$session" || echo "No sessions found."
     }
 fi
 
 fzf-git-switch-branch() {
     # Check if we are in a git repo
     is_in_git_repo || return
+    local target_branch
     target_branch=$(_fzf_choose_branch) && git switch "$target_branch"
+    zle reset-prompt
+    unset target_branch
 }
 
 gr() {
     is_in_git_repo || return
     local RE_FILES="^\sM\s+\K.+"
     local preview_cmd='git diff --color=always {+1} | delta'
-    git restore $(_git_restore_files)
+    git restore "$(_git_restore_files)"
+    unset preview_cmd
+    unset RE_FILES
 }
 
 grs() {
     is_in_git_repo || return
     local RE_FILES="^M[\sMD]+\K.+"
     local preview_cmd='git diff --staged --color=always {+1} | delta'
-    git restore --staged $(_git_restore_files)
+    git restore --staged "$(_git_restore_files)"
+    unset preview_cmd
+    unset RE_FILES
 }
 
 # Open nvim with file(s)
 fzf-open-file-in-editor() {
     # TODO: Add scrolling for the preview
     local querystring
-    [[ "$@" ]] && querystring="$@"
+    [[ "$1" ]] && querystring="$1"
     preview_cmd='bat --number --color=always --paging never {+1}'
     # TODO: Figure out how to open multiple files at once in separate splits
     # Hint: paste -sd " " - will join outputs into a single line
     target_file=$(fd -t f -L -H -E .git/ | _fzf_window --header "Select a file") && ${EDITOR} $target_file
+    zle reset-prompt
 }
 
 # Navigate to cirectory from anywhere
 fzf-cd-to-dir() {
     local querystring
-    [[ "$@" ]] && querystring="$@"
+    [[ "$1" ]] && querystring="$1"
     preview_cmd='exa --icons -T -L 1 --group-directories-first --git --git-ignore --colour=always {+1}'
-    target_dir=$(fd --full-path $HOME -t d $HOME -L | _fzf_window --header "Select a directory") &&
-        cd $target_dir
+    target_dir=$(fd --full-path "$HOME" -t d "$HOME" -L | _fzf_window --header "Select a directory") &&
+        cd "$target_dir" || return
+    zle reset-prompt
+    unset querystring
+    unset target_dir
 }
