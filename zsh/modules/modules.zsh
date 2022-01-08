575f08b581f98546c09779306d2b5f66f15a26b0
Ferran Jovell <ferran@jovell.dev>
2022-01-08 21:25:49 +0100

    [zsh] Removed unused environment modules

--- zsh/modules/modules.zsh
+++
@@ -1,6 +0,0 @@
-# Export Modulepath variable
-HOME_MODULEPATH="$DOTFILES/Modules/modulefiles"
-[[ -d $HOME_MODULEPATH && ! $MODULEPATH =~ $HOME_MODULEPATH ]] &&
-    export MODULEPATH="$MODULEPATH:$HOME_MODULEPATH"
-
-[ "$(command -v module)" ] && module load git/latest python/latest
