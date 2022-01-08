955db632a929701d406b8f7312e93f6bae204c17
Ferran Jovell <ferran@jovell.dev>
2022-01-08 21:32:15 +0100

    [zsh/fzf] Refactor a bit the fzf functions and use better names

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -13,24 +13,24 @@ export FZF_DEFAULT_OPTS="--ansi --reverse --border --height 80% --exact"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # source: https://junegunn.kr/2016/07/fzf-git/
-is_in_git_repo() {
+is-in-git-repo() {
     git rev-parse --is-inside-work-tree &>/dev/null
 }
 
 # Default FZF opts and parameters
-_fzf_window() {
+-fzf-custom-window() {
     local query
     [[ "$querystring" ]] && query="-q ${querystring}"
-    fzf-tmux --exit-0 --select-1 --preview "$preview_cmd" --select-1 $query "$@"
+    fzf-tmux --exit-0 --select-1 --preview "$preview_cmd" $query "$@"
 }
 
-_git_restore_files() {
-    git status -s | grep -oP "$RE_FILES" | _fzf_window --header "Select a file" --layout=reverse -m --preview-window=right:80%
+-fzf-git-restore_files() {
+    git status -s | grep -oP "$RE_FILES" | -fzf-custom-window --header "Select a file" --layout=reverse -m --preview-window=right:80%
 }
 
-_fzf_choose_branch() {
+-fzf-git-choose-branch() {
     local preview_cmd='git lol --color=always -20 {+1}'
-    git branch --list | grep -oP "^\s+\K.+$" | _fzf_window --header "Select a branch" --height 80% --preview-window=down:75%
+    git branch --list | grep -oP "^\s+\K.+$" | -fzf-custom-window --header "Select a branch" --height 80% --preview-window=down:75%
 }
 
 # source: https://github.com/junegunn/fzf/wiki/Examples#tmux
@@ -57,57 +57,57 @@ if [ "$(command -v tmux)" ]; then
                 fzf --select-1 --exit-0 --height "$(tmux -f "$TMUX_CONFIG" list-sessions | wc -l)"
         ) &&
             tmux -f "$TMUX_CONFIG" "$change" -t "$session" || echo "No sessions found."
+        unset TMUX_CONFIG
+        unset session
     }
 fi
 
-fzf-git-switch-branch() {
-    # Check if we are in a git repo
-    is_in_git_repo || return
-    local target_branch
-    target_branch=$(_fzf_choose_branch) && git switch "$target_branch"
-    zle reset-prompt
-    unset target_branch
-}
-
 gr() {
-    is_in_git_repo || return
+    is-in-git-repo || return
     local RE_FILES="^\sM\s+\K.+"
     local preview_cmd='git diff --color=always {+1} | delta'
-    git restore "$(_git_restore_files)"
+    git restore "$(-fzf-git-restore_files)"
     unset preview_cmd
     unset RE_FILES
 }
 
 grs() {
-    is_in_git_repo || return
+    is-in-git-repo || return
     local RE_FILES="^M[\sMD]+\K.+"
     local preview_cmd='git diff --staged --color=always {+1} | delta'
-    git restore --staged "$(_git_restore_files)"
+    git restore --staged "$(-fzf-git-restore_files)"
     unset preview_cmd
     unset RE_FILES
 }
 
-# Open nvim with file(s)
+fzf-git-switch-branch() {
+    is-in-git-repo || return
+    local target_branch
+    target_branch=$(-fzf-git-choose-branch) && git switch "$target_branch"
+    zle reset-prompt
+    unset target_branch
+}
+
 fzf-open-file-in-editor() {
-    # TODO: Add scrolling for the preview
     local querystring
     [[ "$1" ]] && querystring="$1"
     preview_cmd='bat --number --color=always --paging never {+1}'
     # TODO: Figure out how to open multiple files at once in separate splits
     # Hint: paste -sd " " - will join outputs into a single line
-    target_file=$(fd -t f -L -H -E .git/ | _fzf_window --header "Select a file")
+    target_file=$(fd -t f -L -H -E .git/ | -fzf-custom-window --header "Select a file")
     test "$target_file" && "${EDITOR}" "$target_file"
     zle reset-prompt
 }
 
-# Navigate to cirectory from anywhere
 fzf-cd-to-dir() {
     local querystring
     [[ "$1" ]] && querystring="$1"
     preview_cmd='exa --icons -T -L 1 --group-directories-first --git --git-ignore --colour=always {+1}'
-    target_dir=$(fd --full-path "$HOME" -t d "$HOME" -L | _fzf_window --header "Select a directory") &&
-        test "$target_dir" && cd "$target_dir" || return
-    zle reset-prompt
+    target_dir=$(fd --full-path "$HOME" -t d "$HOME" -L | -fzf-custom-window --header "Select a directory") &&
+        test "$target_dir"
     unset querystring
+    zle push-line
+    cd "$target_dir" || return
     unset target_dir
+    zle reset-prompt
 }
