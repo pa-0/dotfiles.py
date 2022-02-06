4be90a09382873e532fd636cacd1c3147e94add0
Ferran Jovell <ferran@jovell.dev>
2022-02-06 15:39:10 +0100

    [zsh] FINALLY make zsh history work

--- zsh/modules/history.zsh
+++ zsh/modules/history.zsh
@@ -4,51 +4,54 @@
 # See:
 #   1. http://zsh.sourceforge.net/Doc/Release/Options.html#History
 
+HISTSIZE=50000
+SAVEHIST=10000
+
 # Each ZSH session appends it's history list to the history file, rather than replacing it.
-setopt APPEND_HISTORY
+setopt append_history
 
 # Save each command's beginning Unix timestamp and the duration in seconds.
-setopt EXTENDED_HISTORY
+setopt extended_history
 
 # Remove the oldest history event that has a duplicate first before unique events when reaching the size limit.
-setopt HIST_EXPIRE_DUPS_FIRST
+setopt hist_expire_dups_first
 
 # Hide duplicates of previous events when searching for history entries in the line editor.
-setopt HIST_FIND_NO_DUPS
+setopt hist_find_no_dups
 
 # Don't store events if they are duplicates of the previous event.
-setopt HIST_IGNORE_DUPS
+setopt hist_ignore_dups
 
 # Don't store events when the first character on the line is a space or when one of the expanded aliases contains
 # a leading space.
 # Note that the command is still kept in the internal history until the next command before it vanishes,
 # allowing to briefly reuse or edit the line.
-setopt HIST_IGNORE_SPACE
+setopt hist_ignore_space
 
 # Don't store function definitions.
 # Note that the command is still kept in the internal history until the next command before it vanishes,
 # allowing to briefly reuse or edit the line.
-setopt HIST_NO_FUNCTIONS
+setopt hist_no_functions
 
 # Remove superfluous blanks from events being added to the history list.
 # This can also prevent duplicate entries when a previous event would not match in its untidied form.
 # Note that the shell is smart enough not to remove blanks which are important, i.e. when quoted.
-setopt HIST_REDUCE_BLANKS
+setopt hist_reduce_blanks
 
 # Don't store duplicate event at all, older commands that duplicate newer ones are omitted.
-setopt HIST_SAVE_NO_DUPS
+setopt hist_save_no_dups
 
 # show command with history expansion to user before running it
-setopt HIST_VERIFY
+setopt hist_verify
 
 # New history lines are added to `HISTFILE` incrementally as soon as they are entered,
 # rather than waiting until the shell exits.
-setopt INC_APPEND_HISTORY
+setopt inc_append_history
 
 # Disable beep in ZLE when a widget attempts to access a non-existing history entry.
-setopt NO_HIST_BEEP
+setopt no_hist_beep
 
 # share command history data
-setopt SHARE_HISTORY
+setopt share_history
 #
 # vi: ft=zsh
