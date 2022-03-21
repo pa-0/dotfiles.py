02223fb3b1975e5e104489a29d75fcdcd54424dc
Ferran Jovell <ferran.jovell@hybridtheory.com>
2022-03-21 12:45:02 +0100

    [nvim] Adds ScreenShare command
    Source; https://gist.github.com/teddylear/30ad1a82fbd114fe01b1ba8d36f16aae

--- nvim/lua/custom_functions.lua
+++ nvim/lua/custom_functions.lua
@@ -12,4 +12,42 @@ function M.notes_cmd()
     -- vim.api.nvim_set_current_dir(cwd)
 end
 
+M.screen_share = function()
+    local Path = require("plenary.path")
+    local alacritty_config_path_string = "~/.config/alacritty/alacritty.yml"
+    local alacritty_config_path = Path:new(alacritty_config_path_string)
+    local alacritty_file_contents = alacritty_config_path:readlines()
+    local size_line_index
+    for i, line in ipairs(alacritty_file_contents) do
+        -- Check for contents of line and see if size is there
+        if string.find(line, "size") then
+            size_line_index = i
+        end
+    end
+    local size_config_string = alacritty_file_contents[size_line_index]
+
+    -- split string
+    local t = {}
+    for str in string.gmatch(size_config_string, "([^" .. " " .. "]+)") do
+        table.insert(t, str)
+    end
+
+    -- size value is always second value
+    local current_size = tonumber(t[2])
+    local big_size = 20.0
+    local small_size = 14.5
+    if current_size == small_size then
+        alacritty_file_contents[size_line_index] = "  size: " .. big_size
+        vim.wo.relativenumber = false
+    elseif current_size == big_size then
+        alacritty_file_contents[size_line_index] = "  size: " .. small_size
+        vim.wo.relativenumber = true
+    else
+        error("Current size is not expected value:", current_size)
+    end
+
+    local alacritty_file_string = table.concat(alacritty_file_contents, "\n")
+    alacritty_config_path:write(alacritty_file_string, "w")
+end
+
 return M
