6422b3f725f744b4f498e8d45c05e300b1eaf219
Ferran Jovell <ferran@jovell.dev>
2022-05-07 13:10:05 +0200

    [fzf] Cleanup

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -9,6 +9,4 @@ export FZF_DEFAULT_OPTS="--ansi --reverse --border --height 80% --exact --exit-0
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-FZF_MODULES="$DOTFILES/fzf/modules"
-
-for module ($FZF_MODULES/*.zsh) source $module
+for module ($DOTFILES/fzf/modules/*.zsh) source $module
