fae02a67af1b8798aa6377828b02479573cf1145
Ferran Jovell <ferran.jovell@hybridtheory.com>
2022-05-23 09:53:55 +0200

    [zsh] Adds new alias for aws
    - Use awshelp as an alias for aws
    - Refactor the alias file
    - cl -> c

--- zsh/modules/aliases.zsh
+++ zsh/modules/aliases.zsh
@@ -4,7 +4,7 @@ alias please='sudo'
 alias prettyplease='sudo !!'
 alias less='less -r'
 alias untar='tar xvfz'
-alias cl='clear'
+alias c='clear'
 alias copy='xsel -i --clipboard' # Meant to use it as a pipe
 alias cd-='cd -'
 alias e="\$EDITOR --noplugins"
@@ -16,7 +16,13 @@ alias rm='rm -v'
 alias cp='cp -v'
 alias mkdir='mkdir -pv'
 
-# Replace ls for exa
+alias -s html="\$BROWSER"
+
+[ "$(command -v bat)" ] && alias cat='bat'
+[ "$(command -v ggrep)" ] && alias grep='ggrep'
+[ "$(command -v awshelp)" ] && alias aws=awshelp
+[ "$(command -v nitrogen)" ] && alias wallpaper='nitrogen --restore'
+
 if [ "$(command -v exa)" ]; then
     alias ls='exa'
     alias ll='exa --icons --long --git'
@@ -27,34 +33,15 @@ else
     alias ll='ls -lsF'
 fi
 
-# Replace cat for bat
-[ "$(command -v bat)" ] && alias cat='bat'
-
-# Aliases for dnf
 if [ "$(command -v dnf)" ]; then
     alias dnfl='dnf list'
     alias dnfli='dnf list installed'
     alias dnfp='dnf info'
     alias dnfs='dnf search'
     alias dnfw='dnf whatprovides'
-
     alias dnfu='sudo dnf update'
     alias dnfy='sudo dnf update -y'
     alias dnfi='sudo dnf install'
     alias dnfyy='sudo dnf install -y'
     alias dnfr='sudo dnf remove'
 fi
-
-if [ "$(command -v ggrep)" ]; then
-    alias grep='ggrep'
-fi
-
-if [ "$(command -v nitrogen)" ]; then
-    alias wallpaper='nitrogen --restore'
-fi
-
-alias ..='cd ..'
-alias ...='cd ../../'
-alias ....='cd ../../..'
-
-alias -s html="\$BROWSER"
