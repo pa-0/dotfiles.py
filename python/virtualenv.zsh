4d63c43e9dbc03cbaab331ffb6ee1912e15d2e05
Ferran Jovell <ferran.jovell@hybridtheory.com>
2022-09-28 09:52:14 +0200

    [virtualenv] Simplify mkv, improve postmkvirtualenv

--- python/virtualenv.zsh
+++ python/virtualenv.zsh
@@ -4,15 +4,7 @@ VIRTUALENVWRAPPER_HOOK_DIR=$DOTFILES/python/virtualenvwrapper
 VIRTUALENVWRAPPER_SCRIPT=$HOME/.local/bin/virtualenvwrapper.sh
 PIP_REQUIRE_VIRTUALENV=true
 
-mkv() {
-    local PYTHON_VERSION
-    PYTHON_VERSION=$1
-    if [[ -z $PYTHON_VERSION ]]; then
-        PYTHON_VERSION="$(command -v python3)"
-    fi
-    mkvirtualenv --python="$PYTHON_VERSION" -a "$PWD" "${PWD##*/}"
-}
-
+alias mkv='mkvirtualenv -a $PWD ${PWD##*/}'
 alias rmv='deactivate && rmvirtualenv ${PWD##*/}'
 alias a='workon ${PWD##*/}'
 alias d='deactivate'
