4f2312104928aaeb5f32eb6dab55040c7ebae254
Ferran Jovell <ferran@jovell.dev>
2023-01-07 12:58:04 +0100

    [nvim] Add modes.nvim plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -45,6 +45,14 @@ require("packer").startup(function(use)
     })
 
     use({
+        "mvllow/modes.nvim",
+        tag = "v0.2.0",
+        config = function()
+            require("plugins.modes").setup()
+        end,
+    })
+
+    use({
         "lukas-reineke/indent-blankline.nvim",
         config = function()
             require("plugins.blankline").setup()
