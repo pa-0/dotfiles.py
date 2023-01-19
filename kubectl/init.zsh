457578b4cdcd221a7c9e583e384c401d4353c047
Ferran Jovell <ferran.jovell@hybridtheory.com>
2023-01-19 09:44:31 +0100

    [shell] Remove kubectl config
    Not used anymore

--- kubectl/init.zsh
+++
@@ -1,9 +0,0 @@
-# Kubectl config
-
-export KUBECONFIG="$HOME/.config/kube/config"
-export KREW_ROOT="$HOME/.local/share/krew"
-
-# TODO: Find what fixes this problem :(
-if [[ "$(command -v kubectl)" ]]; then
-    source <(kubectl completion zsh)
-fi
