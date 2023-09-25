d930de9329132fc6df370abcb02d169b4c90410e
Ferran Jovell <ferran@jovell.dev>
2023-09-25 20:31:51 +0200

    [nvim] Remove unused plugins
    - Neogit
    - duck.nvim

--- nvim/lua/plugins/duck.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "tamton-aquib/duck.nvim",
-    config = function()
-        vim.cmd("command! DuckHatch lua require('duck').hatch()")
-        vim.cmd("command! DuckCook lua require('duck').cook()")
-    end,
-    cmd = { "DuckHatch", "DuckCook" },
-}
