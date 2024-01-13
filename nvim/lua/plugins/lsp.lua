4d25a33ad257f32090ca494c630fb18879cfcc4b
Ferran Jovell <ferran@jovell.dev>
2024-01-13 18:44:43 +0100

    [nvim] Remove null_ls as dependency
    - Format python with pylsp directly with the extensions.
    - Add more hooks to pre-commit
    - Add additional linting rules to selene config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,4 +1,21 @@
-local function borders()
+local function on_attach(_client, bufnr)
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    local set_keymap = vim.keymap.set
+
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    set_keymap("n", "gd", vim.lsp.buf.definition, opts)
+    set_keymap("n", "gr", vim.lsp.buf.rename, opts)
+    set_keymap("n", "gR", vim.lsp.buf.references, opts)
+    set_keymap("n", "gq", vim.lsp.buf.format, opts)
+    set_keymap("n", "K", vim.lsp.buf.hover, opts)
+    set_keymap("n", "gl", vim.diagnostic.setloclist, opts)
+end
+
+local function setup()
+    local lspconfig = require("lspconfig")
+
+    vim.diagnostic.config({ virtual_text = false })
     -- Set up border for floating preview windows
     -- source: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
     local border = {
@@ -13,49 +30,14 @@ local function borders()
     }
 
     local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
     function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-        opts = opts or {}
-        opts.border = opts.border or border
+        opts.border = border
         return orig_util_open_floating_preview(contents, syntax, opts, ...)
     end
-end
 
-local function on_attach(client, bufnr)
-    client.server_capabilities.documentFormattingProvider = false
-
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-    local opts = { noremap = true, silent = true, buffer = bufnr }
-    local set_keymap = vim.keymap.set
-
-    set_keymap("n", "gd", vim.lsp.buf.definition, opts)
-    set_keymap("n", "gr", vim.lsp.buf.rename, opts)
-    set_keymap("n", "gR", vim.lsp.buf.references, opts)
-    set_keymap("n", "K", vim.lsp.buf.hover, opts)
-end
-
-local function disable_virtual_text()
-    vim.diagnostic.config({ virtual_text = false })
-end
-
-local function setup_lsp(lsp_name)
-    require("lspconfig")[lsp_name].setup({ on_attach = on_attach })
-end
-
-local function setup_all_servers(servers)
-    for _, lsp in ipairs(servers) do
-        setup_lsp(lsp)
-    end
-end
-
-local function setup()
-    disable_virtual_text()
-    borders()
-    setup_all_servers({
-        "pylsp",
-        "tailwindcss",
-        "tsserver",
-    })
+    lspconfig.pylsp.setup({ on_attach = on_attach })
+    lspconfig.tailwindcss.setup({ on_attach = on_attach })
 end
 
 return {
@@ -63,8 +45,5 @@ return {
     config = function()
         setup()
     end,
-    dependencies = {
-        "none_ls",
-    },
     event = "BufRead",
 }
