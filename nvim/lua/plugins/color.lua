64117daec42e3a266894b27132a752593c7545eb
Ferran Jovell <ferran@jovell.dev>
2024-03-04 14:19:51 +0100

    [nvim] Change comment color for tokyonight night

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -3,6 +3,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require("tokyonight").setup({
+            on_colors = function(colors)
+                colors.comment = "#9aa5ce"
+            end,
+        })
         vim.cmd([[colorscheme tokyonight-storm]])
     end,
 }
